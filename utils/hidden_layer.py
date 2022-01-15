9f810aabbd20e674fdd224de6e92c1f8c978e05c
kirill <rroogg76aacchh@gmail.com>
2022-01-15 16:39:43 +0300

    Link layers with each other

--- utils/hidden_layer.py
+++ utils/hidden_layer.py
@@ -13,18 +13,23 @@ class HiddenLayerDense:
         self.weights = weights
         self.bias_weights = bias_weights
         self.function_output = []
+        self.function_input = []
         for func in range(len(bias_weights)):
             list_function = []
             list_function.append(Const(bias_weights[func]))
             for i in range(weights[func]):
                 list_function.append(Polynome(i, weights[func, i], None, 1))
             p = Plus(list_function)
+            self.function_input.append(list_function)
             self.function_output.append(construct_function(type, p, func))
 
     def link_layer_as_prev(self, prev_hidden_layer):
         prev = prev_hidden_layer.function_output  # calc number of functions out
-        curr = self.function_output  # calc number of arguments in
-        if (len(prev) != len(curr)):
-            raise Exception('wrong_join_size', "Prev: " + str(prev.size) + "Curr: " + str(curr.size))
+        curr = self.function_input  # calc number of arguments in
+        if len(prev) != len(curr):
+            raise Exception('wrong_join_size', "Prev: " + str(len(prev)) + "Curr: " + str(len(curr)))
+        for fs in curr:
+            for func in fs:
+                func.func = prev[func.pos]
         # for i in range(prev.size):
         # change variables to functions, which have the same numeber of elements
