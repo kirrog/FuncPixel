1fdd55d613c76d0bc5b91fbf5df35ce1d0a05ed5
Kirill <rroogg76aacchh@gmail.com>
2022-02-11 01:55:42 +0300

    Picture optimization

--- finding/find.py
+++ finding/find.py
@@ -1,34 +1,20 @@
 import numpy
 import tensorflow as tf
 
-coef = 1.0
-
-
-def calc_model_point_grad(model, point, pos):
-    with tf.GradientTape() as tape:
-        y_r = model(point)
-        x_n = numpy.zeros(10, dtype=numpy.float32)
-        x_n[pos] = 1.0
-        x = tf.Variable(x_n)
-        y = y_r * x
-    dy_dx = tape.gradient(y, point)
-    return dy_dx, y_r
-
-
-def make_move(bef, aft):  # maximize probability of class, so use +
-    res = bef + aft * coef
-    res[res > 1.0] = 1.0
-    res[res < 0.0] = 0.0
-    return res
+learning_rate = 0.1
 
 
 def find_supr(model, point, pos):
-    p = point
-    d, r = calc_model_point_grad(model, p, pos)
-    r_p = None
-    for i in range(1000):
-        d, r_p = calc_model_point_grad(model, p, pos)
-        if d.numpy().max() < 0.05:
+    opt = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    p_curr = tf.Variable(point)
+    p_prev = tf.Variable(point)
+    loss = lambda: -model(p_curr)[0, pos]
+    r_fir = -loss()
+    for i in range(int(2 / learning_rate)):
+        opt.minimize(loss, [p_curr])
+        p_diff = p_prev - p_curr
+        if p_diff.numpy().max() < 0.05:
             break
-        p = make_move(p, d)
-    return p, r_p, r
+        p_prev = p_curr
+    r_res = -loss()
+    return r_fir, r_res, p_curr
--- test.py
+++ test.py
@@ -3,12 +3,12 @@ import tensorflow as tf
 
 from finding.find import find_supr
 
-# model = tf.keras.models.load_model('data/models/epochs/ep008-loss0.023-accuracy0.992_20211127-220304.h5')
-# model.summary()
-#
-# x = tf.Variable(np.zeros((1, 28, 28, 1)))
-#
-# p, r_p, r = find_supr(model, x, 0)
-#
-# print(r_p.numpy())
-# print(r.numpy())
+model = tf.keras.models.load_model('data/models/epochs/ep008-loss0.023-accuracy0.992_20211127-220304.h5')
+model.summary()
+
+x = tf.Variable(np.zeros((1, 28, 28, 1)))
+
+r_fir, r_res, p_curr = find_supr(model, x, 0)
+
+print(r_fir)
+print(r_res)
