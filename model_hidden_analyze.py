df5b41da2f787bcb3bdebb52e997831dd67c10e7
Kirill <rroogg76aacchh@gmail.com>
2022-03-20 13:41:28 +0300

    Model weights change

--- model_hidden_analyze.py
+++ model_hidden_analyze.py
@@ -1,3 +1,4 @@
+import numpy as np
 from tensorflow import keras
 import tensorflow as tf
 
@@ -7,6 +8,32 @@ def create_hidden_output_layer(model):
                             outputs=[layer.output for layer in model.layers])
     return extractor
 
+
+def change_weights_of_hidden_layer(ouput, weights_matrix, weights_shift):
+    avg = np.average(ouput)
+    to_del = []
+    to_save = []
+    for i in range(ouput.shape[1]):
+        if ouput[0, i] > avg:
+            weights_shift[i] = 0
+            weights_matrix[:, i] = 0  # check if its right
+
+
+tf.config.set_visible_devices([], 'GPU')
 model = tf.keras.models.load_model('data/models/epochs/ep008-loss0.023-accuracy0.992_20211127-220304.h5')
 model.summary()
 model_hidden = create_hidden_output_layer(model)
+
+test = np.zeros((1, 28, 28, 1))
+res = model_hidden(test)
+print(res[-1])
+a = model.get_weights()
+for i in range(2, len(res) - 1):
+    print(res[i].shape)
+    change_weights_of_hidden_layer(res[i], a[(i - 2) * 2], a[(i - 2) * 2 + 1])
+model.set_weights(a)
+res = model_hidden(test)
+print(res[-1])
+# model.weights[6][]
+# res = model(test)
+# print(res)
--- test.py
+++ test.py
@@ -5,17 +5,20 @@ import numpy as np
 from tensorflow import keras
 import tensorflow as tf
 
-from data_work.data_loader import pict_size, load_grad_local_minims
+from data_work.data_loader import pict_size, load_grad_local_minims, load_local_vals
 
-data = load_grad_local_minims()
+data = load_local_vals()
 limit = 0.0005
 coef = 1000000
 print("Data loaded")
+print("Max: " + str(data.max()))
+print("Min: " + str(data.min()))
+exit()
 res = np.zeros((data.shape[1], int(limit * coef)), dtype=int)
 num = np.zeros(data.shape[1])
 for i in range(data.shape[0]):
     for j in range(data.shape[1]):
-        grad = abs(data[i, j, 0]) + abs(data[i, j, 1])
+        grad = abs(data[i, j, 0]) + abs(data[i, j, 1]) / 2
         if grad < limit:
             res[j, int(grad * coef)] += 1
         else:
@@ -27,9 +30,9 @@ for j in range(data.shape[1]):
     plt.xlabel("Positions")
     plt.ylabel("Points number")
     plt.savefig(
-        "classes_minimums_grads/class{c:01d}/ limit: {p:05f} coef: {coef:05d}.jpg".format(c=j, p=limit, coef=coef))
+        "classes_minimums_vals/class{c:01d}/ limit: {p:05f} coef: {coef:05d}.jpg".format(c=j, p=limit, coef=coef))
     plt.figure().clear()
-f = open("results/point_gradient_analysis.md", "a")
+f = open("results/point_values_analysis.md", "a")
 f.write("| {limit:03f} | {coef:010d} ".format(limit=limit, coef=coef))
 for i in num:
     f.write("| {n:05d} ".format(n=int(i)))
